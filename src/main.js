b856a5728c184d02a971bf6a163987dd24eb662e
Artem Kuptsov <kuptsov210@gmail.com>
2021-08-20 12:22:57 +0300

    Edit main files

--- src/main.js
+++ src/main.js
@@ -4,20 +4,42 @@ import "./styles.css";
 import ProductList from "./components/ProductList";
 import Product from "./components/Product";
 import CartButton from "./components/cart/CartButton";
+import CartModal from "./components/cart/CartModal";
 
 const app = createApp({
     data: () =>({
         cartProducts: [],
     }),
+    computed: {
+        totalAmount() {
+            return this.cartProducts.length;
+        }
+    },
     methods:{
         addToCart(product){
-            console.log('products main')
-            this.cartProducts.push(product)
+            this.cartProducts.push({...product, amount: 1})
             console.log(this.cartProducts)
+        },
+        isInCart(id) {
+            return !!this.cartProducts.find((item) => item.id === id);
+        },
+        removeProduct(index) {
+          this.cartProducts.splice(index, 1)
+        },
+        addMore(index) {
+            const product = this.cartProducts[index];
+            this.cartProducts.splice(index, 1, {...product, amount: product.amount + 1});
+        },
+        deleteOne(index) {
+            const product = this.cartProducts[index];
+            if (product.amount > 1) {
+                this.cartProducts.splice(index, 1, {...product, amount: product.amount - 1});
+            }
         }
     }
 });
 app.component('cart-button', CartButton)
+app.component('cart-modal', CartModal)
 app.component('product-list', ProductList)
 app.component('product', Product)
 app.mount("#app");
